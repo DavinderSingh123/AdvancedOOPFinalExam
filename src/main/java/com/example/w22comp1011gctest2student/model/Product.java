5315254b27c1d9ed19b44a81f871e32468935a90
DavinderSingh123 <200484320@student.georgianc.on.ca>
2022-08-18 09:49:24 -0400

    Question 2 complete

--- src/main/java/com/example/w22comp1011gctest2student/model/Product.java
+++ src/main/java/com/example/w22comp1011gctest2student/model/Product.java
@@ -1,5 +1,44 @@
 package com.example.w22comp1011gctest2student.model;
 
+// Product Class
 public class Product {
 
+    //Member Variables
+    private int id;
+    private String sku;
+    private String name;
+    private double salePrice;
+    private double regularPrice;
+    private String image;
+
+    // Getter methods
+    public int getId() {
+        return id;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalePrice() {
+        return salePrice;
+    }
+
+    public double getRegularPrice() {
+        return regularPrice;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    // Overridden to String method
+    @Override
+    public String toString() {
+        return name + "-$" + salePrice;
+    }
 }
